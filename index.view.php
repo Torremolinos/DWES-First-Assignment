da14da4f85512f57fdacbfa784bcd532fd09d259
Torremolinos <adrian_2054@hotmail.com>
2023-10-25 11:50:07 +0200

    Añado los metodos para la realizacion del ejercicio

--- index.view.php
+++ index.view.php
@@ -8,14 +8,65 @@
 </head>
 
 <body>
-    <h1>Gente suscrita
-    <ol>
-        <?php foreach ($clientes as $cliente) : ?>
-            <li>
-            <?= $cliente['Nombre']; ?> (<?= $cliente['Lista de pedidos']; ?>), por <?= $cliente['autor'] ?>
-           
-            </li>
-            <?php endforeach; ?>
-    </ol>
-    </h1>
+            <?php
+
+            $clientesSuscritos = array_filter($clientes, function ($cliente) {
+                return $cliente["Cliente suscrito"] === true;
+            });
+
+            $nombresClientesSuscritos = array_map(function ($cliente) {
+                return $cliente["Nombre"];
+            }, $clientesSuscritos);
+
+            echo "<h2>Clientes Suscritos:</h2>";
+            echo "<ul>";
+            foreach ($nombresClientesSuscritos as $nombre) {
+                echo "<li>$nombre</li>";
+            }
+            echo "</ul>";
+            ?>
+            <?php
+            // Filtrar clientes no suscritos
+            $clientesNoSuscritos = array_filter($clientes, function ($cliente) {
+                return $cliente["Cliente suscrito"] === false;
+            });
+
+            $nombresClientesNoSuscritos = array_map(function ($cliente) {
+                return $cliente["Nombre"];
+            }, $clientesNoSuscritos);
+
+            echo "<h2>Clientes No Suscritos:</h2>";
+            echo "<ul>";
+            foreach ($nombresClientesNoSuscritos as $nombre) {
+                echo "<li>$nombre</li>";
+            }
+            echo "</ul>";
+            ?>
+            <?php
+            // Filtrar clientes con al menos un pedido
+            $clientesConPedidos = array_filter($clientes, function ($cliente) {
+                return isset($cliente["Listado de pedidos"]);
+            });
+
+            // Calcular el importe total de los pedidos para cada cliente
+            $clientesConImporteTotal = array_map(function ($cliente) {
+                $importeTotal = 0;
+                if (isset($cliente["Listado de pedidos"])) {
+                    foreach ($cliente["Listado de pedidos"] as $pedido) {
+                        if (isset($pedido["Precio"])) {
+                            $importeTotal += $pedido["Precio"];
+                        }
+                    }
+                }
+                return ["Nombre" => $cliente["Nombre"], "Importe Total" => $importeTotal];
+            }, $clientesConPedidos);
+
+            echo "<h2>Clientes con Pedidos y su Importe Total:</h2>";
+            echo "<ul>";
+            foreach ($clientesConImporteTotal as $cliente) {
+                echo "<li>{$cliente['Nombre']} (Importe Total: {$cliente['Importe Total']})</li>";
+            }
+            echo "</ul>";
+            ?>
+
 </html>
